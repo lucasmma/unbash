4b9aa56f154bcd3d7b07a5168e2e79b0eb029652
Lucas Mendonça <lucasmmamaral@gmail.com>
2022-02-07 20:39:31 -0300

    feat: add parser function

---
+++ src/utils/parser_helper.rs
@@ -0,0 +1,15 @@
+use crate::domain::model::command::Command;
+
+pub fn parse_line(line: String) -> Vec<Command>{
+  let pipe_sections = line.split("|");
+  let mut pipe_sections_parsed: Vec<Command> = vec![];
+  for pipe_section in pipe_sections {
+    let mut commands: Vec<String> = pipe_section.split_whitespace().map(|s| s.to_string()).collect();
+    let command_name: String = commands[0].clone();
+    println!("{}", command_name);
+    commands.remove(0);
+    pipe_sections_parsed.push(Command { command_name: command_name.to_string(), args: commands});
+  }
+
+  return pipe_sections_parsed;
+}
