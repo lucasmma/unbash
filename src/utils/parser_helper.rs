43dda18c7f4262c3d00ce99ee9deeac6e2fc0326
Lucas Mendonça <lucasmmamaral@gmail.com>
2022-02-07 20:42:25 -0300

    refactor: remove println

--- src/utils/parser_helper.rs
+++ src/utils/parser_helper.rs
@@ -6,7 +6,6 @@ pub fn parse_line(line: String) -> Vec<Command>{
   for pipe_section in pipe_sections {
     let mut commands: Vec<String> = pipe_section.split_whitespace().map(|s| s.to_string()).collect();
     let command_name: String = commands[0].clone();
-    println!("{}", command_name);
     commands.remove(0);
     pipe_sections_parsed.push(Command { command_name: command_name.to_string(), args: commands});
   }
