d2508e6239a5c6ff57333380f83df9db15e0f499
Lucas Mendonça <lucasmmamaral@gmail.com>
2022-02-08 22:37:36 -0300

    feat: create file

--- src/utils/file_helper.rs
+++ src/utils/file_helper.rs
@@ -1,6 +1,7 @@
 use std::fs;
 use std::fs::File;
 use std::io::Error;
+use std::io::Write;
 
 pub fn read_file(filename: String) -> String{
   let file_reader = || -> Result<String, Error> {
@@ -22,5 +23,30 @@ pub fn create_file(filename: String){
 
   if let Err(_err) = file() {
 
+  }
+}
+
+pub fn create_write_file(filename: String, content: String){
+  let file = || -> Result<(), Error> {
+    let mut file = File::create(filename)?;
+    file.write_all(content.as_bytes())?;
+    Ok(())
+  };
+
+  if let Err(_err) = file() {
+
+  }
+}
+
+pub fn delete_file(filename: String) {
+  if fs::metadata(filename.clone()).is_ok(){
+    let file = || -> Result<(), Error> {
+      fs::remove_file(filename)?;
+      Ok(())
+    };
+  
+    if let Err(_err) = file() {
+  
+    }
   }
 }
